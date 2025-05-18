ce8788f3e84a9af45e15f099f3a26588205a4c0c
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-18 14:06:43 +0300

    added django-bootstrap5 & fixed the not desplaying error

--- gadgetgalaxy/product/views.py
+++ gadgetgalaxy/product/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from .forms import ProductForm
 from category.models import Category
 
 # Create your views here.
@@ -14,30 +15,44 @@ from django.db import IntegrityError
 
 def add_product(request):
     if request.method == 'POST':
-        if request.POST['product_name'] and request.POST['product_desc'] and request.POST['product_price'] and request.POST['product_stock'] and request.POST['sku'] and request.POST['category'] and request.FILES['product_image'] is not None:
-            product_data = {
-                'name': request.POST['product_name'],
-                'description': request.POST['product_desc'],
-                'price': request.POST['product_price'],
-                'stock': request.POST['product_stock'],
-                'image': request.FILES['product_image'],
-                'sku': request.POST['sku'],
-                'category_id': request.POST['category']
-            }
-            try:
-                Product.create_product(**product_data)
-                return render(request, 'product_success.html')
-            except IntegrityError:
-                error_message = "Please use a unique SKU."
-                categories = Category.get_all_categories()
-                return render(request, 'add_products.html', {'categories': categories, 'error_message': error_message})
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return render(request, 'product_success.html')
         else:
-            error_message = "Please fill all the fields."
             categories = Category.get_all_categories()
-            return render(request, 'add_products.html', {'categories': categories, 'error_message': error_message})
+            return render(request, 'add_products.html', {'form': form, 'categories': categories})
     else:
+        form = ProductForm()
         categories = Category.get_all_categories()
-        return render(request, 'add_products.html', {'categories': categories})
+        return render(request, 'add_products.html', {'form': form, 'categories': categories})
+
+# def add_product(request):
+#     if request.method == 'POST':
+#         if request.POST['product_name'] and request.POST['product_desc'] and request.POST['product_price'] and request.POST['product_stock'] and request.POST['sku'] and request.POST['category'] and request.FILES['product_image'] is not None:
+#             product_data = {
+#                 'name': request.POST['product_name'],
+#                 'description': request.POST['product_desc'],
+#                 'price': request.POST['product_price'],
+#                 'stock': request.POST['product_stock'],
+#                 'image': request.FILES['product_image'],
+#                 'sku': request.POST['sku'],
+#                 'category_id': request.POST['category']
+#             }
+#             try:
+#                 Product.create_product(**product_data)
+#                 return render(request, 'product_success.html')
+#             except IntegrityError:
+#                 error_message = "Please use a unique SKU."
+#                 categories = Category.get_all_categories()
+#                 return render(request, 'add_products.html', {'categories': categories, 'error_message': error_message})
+#         else:
+#             error_message = "Please fill all the fields."
+#             categories = Category.get_all_categories()
+#             return render(request, 'add_products.html', {'categories': categories, 'error_message': error_message})
+#     else:
+#         categories = Category.get_all_categories()
+#         return render(request, 'add_products.html', {'categories': categories})
 
 def page_not_found(request):
     return render(request, 'notfound.html', status=404)
