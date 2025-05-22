7f85def39d18f612e4f3120dc3fae2e2f88dbd2f
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-05-22 15:01:59 +0300

    add productupdateapi to api.views

--- gadgetgalaxy/product/api/views.py
+++ gadgetgalaxy/product/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from ..models import Product
 from .serlizer import ProductSerializer
@@ -19,3 +20,16 @@ def product_list_api(request):
         else:
             return Response(data={'msg': serializer.errors},
                             status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductUpdateAPI(APIView):
+    def put(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({'msg': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Product updated', 'data': serializer.data}, status=status.HTTP_200_OK)
+        return Response({'msg': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
